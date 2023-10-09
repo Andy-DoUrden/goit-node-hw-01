53cf96e8500bf3deccc288ffb7d8b5edbf41ad21
Andy-DoUrden <111894438+Andy-DoUrden@users.noreply.github.com>
2023-10-10 00:56:36 +0300

    removed comented code

--- contacts.js
+++ contacts.js
@@ -23,18 +23,6 @@ async function getContactById(contactId) {
 async function removeContact(contactId) {
   const contacts = JSON.parse(await fs.readFile(contactsPath));
 
-  // const index = contacts.findIndex((contact) => contact.id === contactId);
-
-  // if (index === -1) {
-  //   return null;
-  // }
-
-  // const [result] = contacts.splice(index, 1);
-
-  // await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-
-  // return result;
-
   const deletedContact = await getContactById(contactId);
 
   const filteredContacts = contacts.filter((contact) => contact.id !== contactId);
